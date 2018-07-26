7a984111e31276949264fbfd070be91fd244915f
josue b <josue.carit@gmail.com>
2018-07-25 23:33:16 -0600

    ss

--- sendemail.php
+++ sendemail.php
@@ -9,21 +9,6 @@
     $Content = "Nombre:" . $Name . "\n Correo:" . $Email . "\n Telefono:" . $Phone . "\n Mensaje:" . $Message;
 
     mail($destinatary, "Pagina web consulta", $Content);
-    echo 
-    '<script language="javascript">alert("Mensaje enviado.!Muchas gracias por visitarnos!.");
-    
-    var Nombre = document.querySelector("#Name");
-    var Email = document.querySelector("#Email");
-    var Phone = document.querySelector("#Phone");
-    var Message = document.querySelector("#Message");
-
-    Nombre.innerHTML = "";
-    Email.innerHTML = "";
-    Phone.innerHTML = "";
-    Message.innerHTML = "";
-
-    </script>'; 
-
     header( 'Location: /index.html' ) ; 
 
 ?>
